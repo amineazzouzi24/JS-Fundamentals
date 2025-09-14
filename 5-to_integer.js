10dc3d00944d458cce5cb4a7a7923e2eec02b0cf
MOHAMED AMINE AZZOUZI <93558172+amineazzouzi24@users.noreply.github.com>
2025-09-14 18:12:59 +0100

    5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,8 @@
-const arg = Progress.arg[2];
-const number = parseInt(arg);
-if (isNaN(number)) {
-    console.log('Not a number');
-}
-else {
-    console.log ('My number' + number);
+const arg = process.argv[2];        
+const number = parseInt(arg, 10);   
+
+if (!isNaN(number)) {               
+  console.log("My number: " + number);
+} else {                           
+  console.log("Not a number");
 }
