16bfa46364e7995626dc343f60f594480cacc7f6
MOHAMED AMINE AZZOUZI <93558172+amineazzouzi24@users.noreply.github.com>
2025-09-14 17:38:14 +0100

    value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,11 +1,7 @@
-const args = process.argv.slice(2);
+const arg = process.argv[2];
 
-if (args.args === 0) {
+if (arg === undefined) {
   console.log("No argument");
-} else if (args.args === 1) {
-  console.log("Argument found");
-  console.log("Value of my argument:", args[0]);
-} else {
-  console.log("Arguments found");
-  console.log("Values of my arguments:", args.join(', '));
+} else { 
+  console.log(arg);
 }
